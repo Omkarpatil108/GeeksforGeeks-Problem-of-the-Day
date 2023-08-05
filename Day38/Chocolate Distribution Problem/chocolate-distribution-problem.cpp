a00875b30881cbe7c27358a5be4a4027c7af931d
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-08-05 18:59:44 +0530

    Add files via upload

---
+++ Day38/Chocolate Distribution Problem/chocolate-distribution-problem.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    sort(a.begin(),a.end(),greater<int>());
+    vector<int>v;
+    int i=0;
+    int k=i+m-1;
+    while(k<n){
+        v.push_back(a[i]-a[k]);
+        i++;
+        k++;
+    }
+    sort(v.begin(),v.end());
+    return v[0];
+    
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
