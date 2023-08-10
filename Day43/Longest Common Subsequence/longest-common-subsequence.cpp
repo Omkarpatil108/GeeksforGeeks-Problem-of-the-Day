e5cb31fc59b4816710a05147fb380dd40228d69f
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-08-10 23:09:58 +0530

    Add files via upload

---
+++ Day43/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+const int mod=1e9+7;
+using namespace std;
+
+// } Driver Code Ends
+// function to find longest common subsequence
+
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string s1, string s2)
+    {
+        // your code here
+        int LCS[n+1][m+1];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0 || j==0){
+                    LCS[i][j]=0;
+                    continue;
+                }
+                
+                if(s1[i-1]==s2[j-1])
+                    LCS[i][j]=LCS[i-1][j-1]+1;
+                else
+                    LCS[i][j]=max(LCS[i-1][j], LCS[i][j-1]);
+            }
+        }
+        return LCS[n][m];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t,n,m;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;          // Take size of both the strings as input
+        string s1,s2;
+        cin>>s1>>s2;        // Take both the string as input
+        Solution ob;
+        cout << ob.lcs(n, m, s1, s2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
