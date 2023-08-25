b0330d7dd233e8264e20bc72010994c2baeb4158
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-08-25 19:19:55 +0530

    Add files via upload

---
+++ Day58/Palindrome String/palindrome-string.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	     int l=0;
+    int h=S.length() - 1;
+    
+    while(h>l){
+        if (S[l++] != S[h--]) {
+                return 0;
+        }
+       
+    }
+  return 1;  
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
