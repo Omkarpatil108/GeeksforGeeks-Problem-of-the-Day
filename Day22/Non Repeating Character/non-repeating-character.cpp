63bc5b4fcde3ab5e1a05a00dcebb61b9389f13b7
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-09-29 21:20:22 +0530

    Add files via upload

---
+++ Day22/Non Repeating Character/non-repeating-character.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the first non-repeating character in a string.
+    char nonrepeatingCharacter(string S)
+    {
+       //Your code here
+       vector<int> freq(26, 0);
+    
+        for (char ch : S)
+        {
+            freq[ch - 'a']++;
+        }
+    
+        for (char ch : S)
+        {
+            if (freq[ch - 'a'] == 1)
+                return ch;
+		}
+    
+        return '$';
+       
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--)
+	{
+	
+	    string S;
+	    cin >> S;
+	    Solution obj;
+        char ans = obj.nonrepeatingCharacter(S);
+        
+        if(ans != '$')
+	    cout << ans;
+        else cout << "-1";
+            
+        cout << endl;
+	    
+	}
+	
+	return 0;
+}
+
+// } Driver Code Ends
