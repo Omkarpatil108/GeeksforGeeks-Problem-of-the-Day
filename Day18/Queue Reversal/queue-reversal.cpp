451f41f29e8647530844c17bcc0492275983147b
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-09-29 21:25:56 +0530

    Add files via upload

---
+++ Day18/Queue Reversal/queue-reversal.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        // add code here.
+        {
+        vector<int> V;
+        while(!q.empty())
+        {
+            V.push_back(q.front());
+            q.pop();
+        }
+        while(V.size())
+        {
+            q.push(V[V.size()-1]);
+            V.pop_back();
+        }
+        return q;
+    }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
