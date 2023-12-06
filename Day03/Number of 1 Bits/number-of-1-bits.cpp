18a370e815298256bbcb236bca1109ba8f652f26
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-12-06 21:42:57 +0530

    Add files via upload

---
+++ Day03/Number of 1 Bits/number-of-1-bits.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+         int c=0;
+        while(N){
+            if(N&1) {
+                c++;
+                N=N>>1;
+            }
+            else{
+                N=N>>1;
+                
+            }
+        }
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
